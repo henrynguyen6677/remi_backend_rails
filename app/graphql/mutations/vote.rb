d565112abd3fec35557454972325f4c0c05563e0
henrynguyen6677 <henrynguyen6677@gmail.com>
2026-04-23 16:54:36 +0700

    Fix smell code

--- app/graphql/mutations/vote.rb
+++ app/graphql/mutations/vote.rb
@@ -13,30 +13,7 @@ module Mutations
       post = Post.find(create_vote_input.post_id)
       raise ApiErrors::Error, ApiErrors::POST_NOT_FOUND unless post
 
-      vote_type = create_vote_input.vote 
-      user_id_str = user.user_id.to_s
-      like_ids = post.like_user_ids || []
-      dislike_ids = post.dislike_user_ids || []
-      if ["UP", "SELF_VOTE_UP"].include?(vote_type)
-        if like_ids.include?(user_id_str)
-          like_ids.delete(user_id_str)
-        else
-          like_ids << user_id_str
-          dislike_ids.delete(user_id_str)
-        end
-      elsif ["DOWN", "SELF_VOTE_DOWN"].include?(vote_type)
-        if dislike_ids.include?(user_id_str)
-          dislike_ids.delete(user_id_str)
-        else
-          dislike_ids << user_id_str  
-          like_ids.delete(user_id_str)  
-        end
-      end
-
-      post.update(
-        like_user_ids: like_ids,
-        dislike_user_ids: dislike_ids
-      )
+      post.toggle_vote(user, create_vote_input.vote)
       post
     end
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,19 @@ class Post < ApplicationRecord
   
   belongs_to :user, foreign_key: :user_id, primary_key: :user_id
   validates :url, :title, :content, presence: true
+
+  def toggle_vote(user, type)
+    uid = user.user_id.to_s
+    self.like_user_ids ||= []
+    self.dislike_user_ids ||= []
+
+    case type
+    when "UP", "SELF_VOTE_UP"
+      like_user_ids.include?(uid) ? self.like_user_ids -= [uid] : (self.like_user_ids |= [uid]; self.dislike_user_ids -= [uid])
+    when "DOWN", "SELF_VOTE_DOWN"
+      dislike_user_ids.include?(uid) ? self.dislike_user_ids -= [uid] : (self.dislike_user_ids |= [uid]; self.like_user_ids -= [uid])
+    end
+
+    save
+  end
 end
